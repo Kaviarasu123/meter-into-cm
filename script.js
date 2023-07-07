78c08703942df9d6b60f1466bdc35568e1e4a39b
Kaviarasu123 <115768925+Kaviarasu123@users.noreply.github.com>
2023-07-07 20:45:29 +0530

    metes into cm

---
+++ script.js
@@ -0,0 +1,12 @@
+cc=()=>{
+    let input=Number(document.getElementById("input").value)
+    let ans=input/2.54
+    let result=document.getElementById("result")
+    result.innerHTML= "the resulte is "+ ans
+}
+sc=()=>{
+    let input2=Number(document.getElementById("input2").value)
+    let answer=input2*100
+    let result2=document.getElementById("result2")
+    result2.innerHTML= "the resulte is "+ answer
+}
